af524a3fe53027faf1acda7f02904980c986e0e2
Rfernando0513 <rfernando0513@gmail.com>
2024-07-29 20:01:59 -0300

    update connection string

--- src/Data/database.js
+++ src/Data/database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongoUri = process.env.MONGO_URI;
+//const mongoUri = process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(mongoUri)
+        await mongoose.connect('mongodb+srv://Fernando0513:Hayashi0513$@auth-jwt.gkxk1t6.mongodb.net/?retryWrites=true&w=majority&appName=AUTH-JWT')
         console.log('Banco conectado');
     } catch (err) {
         console.log('Erro ao conectar ao banco de dados:', err);
